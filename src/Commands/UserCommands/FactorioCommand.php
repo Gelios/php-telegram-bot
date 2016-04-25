ced246f9612188d49b77f0d0467c9c751825982e
Alexander Marchenko <alexander.marchenko@admiralmarkets.com>
2016-04-25 13:38:04 +0300

    test

---
+++ src/Commands/UserCommands/FactorioCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Request;
+
+/**
+ * User "/factorio" command
+ */
+class FactorioCommand extends UserCommand
+{
+    /**#@+
+     * {@inheritdoc}
+     */
+    protected $name = 'factorio';
+    protected $description = 'control the factorio server';
+    protected $usage = '/factorio <text>';
+    protected $version = '1.0.0';
+    /**#@-*/
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        $message = $this->getMessage();
+        $chat_id = $message->getChat()->getId();
+        $text = trim($message->getText(true));
+
+        if ($text === '') {
+            $text = 'Command usage: ' . $this->getUsage();
+        }
+
+        $data = [
+            'chat_id' => $chat_id,
+            'text'    => $text,
+        ];
+
+        return Request::sendMessage($data);
+    }
+}
